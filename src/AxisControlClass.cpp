a3a91e7c318cd7a4f0c4028b8e1a88a2c8cbb229
KangDroid <hyunwoo286@gmail.com>
2019-09-09 16:38:58 +0900

    Simplyfi a bit

--- src/AxisControlClass.cpp
+++ src/AxisControlClass.cpp
@@ -15,14 +15,7 @@ void AxisControlClass::moveX(int length, int speed) {
             digitalWrite(x_motor.retDir(), LOW);
         }
         for (int i = 0; i < STEPS_PER_MM::X * length; i++) { // TODO: Motor naming
-            digitalWrite(y_motor.retStep(), HIGH);
-            usleep(20800/speed);
-            digitalWrite(y_motor.retStep(), LOW);
-
-            digitalWrite(x_motor.retStep(), HIGH);
-            usleep(20800/speed);
-            digitalWrite(x_motor.retStep(), LOW);
-            usleep(20800/speed);
+            this->moveAxisInf(speed);
         }
     } else {
         // NORMAL Printer mechanism like Anet, ETC(NOT SF)
@@ -47,14 +40,7 @@ void AxisControlClass::moveY(int length, int speed) {
             digitalWrite(x_motor.retDir(), HIGH);
         }
         for (int i = 0; i < STEPS_PER_MM::Y * length; i++) { // TODO: Motor naming
-            digitalWrite(y_motor.retStep(), HIGH);
-            usleep(20800/speed);
-            digitalWrite(y_motor.retStep(), LOW);
-
-            digitalWrite(x_motor.retStep(), HIGH);
-            usleep(20800/speed);
-            digitalWrite(x_motor.retStep(), LOW);
-            usleep(20800/speed);
+            this->moveAxisInf(speed);
         }
     } else {
         // NORMAL Printer mechanism like Anet, ETC(NOT SF)
